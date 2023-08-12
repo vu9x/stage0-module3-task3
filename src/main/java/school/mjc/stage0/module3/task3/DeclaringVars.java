978d67fbbe529fbba86b5eeeb70f8c4f046f4f1a
Vu <nguyentuanvu9x@gmail.com>
2023-08-12 23:53:05 +0300

    Added: DeclaringVars.java

--- src/main/java/school/mjc/stage0/module3/task3/DeclaringVars.java
+++ src/main/java/school/mjc/stage0/module3/task3/DeclaringVars.java
@@ -3,5 +3,7 @@ package school.mjc.stage0.module3.task3;
 public class DeclaringVars {
     public static void main(String[] args) {
         //write code here
+        int a, b, c = 10;
+        System.out.println(c);
     }
 }
